21eff64abf54c04c8630369ed61bcb9c06665db3
Colboi <1016886725@qq.com>
2025-07-09 21:18:55 +0800

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: '',
+  basePath: isProd ? '/gob' : '',
+  assetPrefix: isProd ? '/gob/' : '',
 };
 
 const mdxEnhancer = withMDX({
